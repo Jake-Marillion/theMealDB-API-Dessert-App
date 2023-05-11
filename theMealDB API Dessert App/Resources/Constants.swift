1acc0bb38bab18664886dcaada9ed34b536e28ae
Jacob Marillion <jacobmarillion@gmail.com>
2023-05-11 17:31:36 -0600

    Created Constants file.

---
+++ theMealDB API Dessert App/Resources/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  theMealDB API Dessert App
+//
+//  Created by Jacob Marillion on 5/11/23.
+//
+
+import Foundation
+
+struct K {
+        //MARK: - TODO - Clean up
+} //End of struct
