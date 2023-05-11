88f9336cdad4a12ff83e241c5b9c0b76648c764a
Jacob Marillion <jacobmarillion@gmail.com>
2023-05-11 12:37:57 -0600

    Initial Commit

---
+++ theMealDB API Dessert App/Views/Custom Views/DessertTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  DessertTableViewCell.swift
+//  theMealDB API Dessert App
+//
+//  Created by Jacob Marillion on 5/11/23.
+//
+
+import UIKit
+
+class DessertTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
